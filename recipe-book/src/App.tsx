9d17a5697566860fb3d8e40f74a5809a35d7abe6
Recipe App <mozgolom004@gmail.com>
2026-06-18 05:05:58 +0300

    fix: seed категорий до первого рендера (устранение гонки первого запуска)

--- recipe-book/src/App.tsx
+++ recipe-book/src/App.tsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import RecipePage from './pages/RecipePage'
 import EditRecipePage from './pages/EditRecipePage'
 import SettingsPage from './pages/SettingsPage'
-import { seedCategories } from './db/categories'
 
 export default function App() {
-  useEffect(() => { seedCategories() }, [])
   return (
     <Routes>
       <Route element={<Layout />}>
--- recipe-book/src/main.tsx
+++ recipe-book/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { seedCategories } from './db/categories'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+seedCategories().finally(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+  )
+})
